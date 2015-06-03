ec18c2eb409b2d6bdbcc00638ba43f4f2a751efc
alex223124 <dadeveloper@hotmail.com>
2015-06-03 15:28:22 +0000

    Added Todo Lists and Todo Items All Ok

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,7 +1,7 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
   
-  def create_table
+  def create
     @todo_item = @todo_list.todo_items.create(todo_item_params)
     redirect_to @todo_list
   end
